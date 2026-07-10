0fd2bbd488b049d2315e71aaceb6b440fb55c532
Robert Carr <robertcarrcisd@gmail.com>
2026-07-10 14:25:03 -0500

    Add short rib birria pizza recipe

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260710-5";
+const CACHE_VERSION = "20260710-6";
 const CACHE_PREFIX = "recipe-book-";
 const SHELL_CACHE = `recipe-book-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `recipe-book-data-${CACHE_VERSION}`;
